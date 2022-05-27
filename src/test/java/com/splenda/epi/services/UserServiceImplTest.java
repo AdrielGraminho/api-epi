bf003a5538a2e7ab819d6ea244fb6f297b849051
AdrielGraminho <adrielgraminho@gmail.com>
2022-05-26 21:21:17 -0300

    adjust project test runner

--- src/test/java/com/splenda/epi/services/UserServiceImplTest.java
+++ src/test/java/com/splenda/epi/services/UserServiceImplTest.java
@@ -1,8 +1,52 @@
 package com.splenda.epi.services;
 
-import org.mockito.junit.MockitoJUnitRunner;
-import org.junit.runner.RunWith;
+import com.splenda.epi.entities.core.User;
+import com.splenda.epi.entities.exceptions.UserNotFoundException;
+import com.splenda.epi.repository.UserRepository;
+import com.splenda.epi.services.impl.UserServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@RunWidth(MockitoJUnitRunner.class)
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class UserServiceImplTest {
+
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Test
+    public void shouldBeReturnUserWhenFindByUserId(){
+        User user = User.builder().idUser(1l).build();
+        when(userRepository.findById(any())).thenReturn(Optional.of(user));
+
+        User result = userService.findUserById(1l);
+
+        assertEquals(user, result);
+        verify(userRepository, times(1)).findById(any());
+    }
+
+    @Test
+    public void shouldBeReturnExceptionWhenFindByUserIdAndNotFound(){
+        when(userRepository.findById(any())).thenReturn(Optional.empty());
+
+        String keyException = "";
+        try {
+            userService.findUserById(1l);
+        }catch (UserNotFoundException userNotFoundException){
+            keyException = userNotFoundException.getKey();
+        }
+        assertEquals("user.not-found", keyException);
+    }
+
 }
